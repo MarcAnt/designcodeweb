0140c41675f9494b127031d19a2fe413e4b09379
MarcAnt <marc_esq_verb@hotmail.com>
2021-04-05 01:36:34 -0400

    Mas cambios

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,37 +1,47 @@
 import { Link } from 'gatsby';
 import React from 'react'
-
-const menuData = [
-
-    {
-        title: "Courses",
-        icon: "images/icons/courses.svg", 
-        link: "/courses"
-    }, 
-    {
-        title: "Tutorials",
-        icon: "images/icons/tutorials.svg", 
-        link: "/tutorials"
-    }, 
-    {
-        title: "Pricing",
-        icon: "images/icons/pricing.svg", 
-        link: "/pricing"
-    }
-];
+import styled from 'styled-components';
+import {menuData} from '../../data/menuData.js';
+import MenuButtons from '../buttons/MenuButtons'
+import MenuTooltip from '../tooltips/MenuTooltip.js';
 
 export default function Header() {
     return(
-        <>
-            {menuData.map((item, index) => (
-                <Link to={item.link} key={index}>
-                    <p>
-                        <img src={item.icon} alt={item.title} />
-                        {item.title}
-                    </p>
-                </Link>
+        <Wrapper>
+
+            <Link to="/">
+                 <img src="images/logos/logo.svg" alt="Logo"/>
             
-            ))}
-        </>
+            </Link>
+
+            <MenuWrapper count={menuData.length}>
+
+                {menuData.map((item, index) => (
+                  
+                  <MenuButtons item={item} key={index} />
+                
+                ))}
+
+            </MenuWrapper>
+
+            <MenuTooltip />
+        </ Wrapper>
     );
 }
+
+const Wrapper = styled.div`
+    position: absolute;
+    top: 60px;
+    display: grid;
+    grid-template-columns: 44px auto;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 30px;
+`;
+const MenuWrapper = styled.div`
+
+    display: grid;
+    gap: 30px;
+    grid-template-columns: repeat(${props => props.count },auto);
+`;
